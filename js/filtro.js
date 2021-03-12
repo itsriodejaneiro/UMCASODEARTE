25a5cd76621983db4ecba3b7103b78c4ac25cfa2
Ramires <ramiresbaptista@gmail.com>
2021-03-12 19:06:55 -0300

    Adiciona 404 no filtro

--- js/filtro.js
+++ js/filtro.js
@@ -1,36 +1,59 @@
+
 filterSelection("all")
-      function filterSelection(c) {
-        var x, i;
-        x = document.getElementsByClassName("filterDiv");
-        if (c == "all") c = "";
-        for (i = 0; i < x.length; i++) {
-          w3RemoveClass(x[i], "show");
-          if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-        }
-      }
+function filterSelection(c) {
 
-      function w3AddClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split(" ");
-        arr2 = name.split(" ");
-        for (i = 0; i < arr2.length; i++) {
-          if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-        }
-      }
+  //Contador de itens no filtro e referencia ao feedback de vazio
+  //melhor usarmos Id, ja q vamos ficar removendo e adicionando classe não corremos o risco de remover uma classe pela qual essa variável irá procurar
+  let filteredCounter = 0;
+  let emptyFeedbackRef = document.getElementById("emptyFeedback");
 
-      function w3RemoveClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split(" ");
-        arr2 = name.split(" ");
-        for (i = 0; i < arr2.length; i++) {
-          while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);     
-          }
-        }
-        element.className = arr1.join(" ");
-      }
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") {
+    c = ""
+  };
 
-      
+  for (i = 0; i < x.length; i++) {
+
+    w3RemoveClass(x[i], "show");
+
+    if (x[i].className.indexOf(c) > -1) {
+      filteredCounter++;  
+    	w3AddClass(x[i], "show")
+    };
+    
+  }
+
+  //Sempre adicionar a classe de esconder no feedback de vazio, p/ previnir que ele nao apareça em momentos indesejados
+  emptyFeedbackRef.classList.add('hide');
+
+  //Caso o contador de items no filtro seja 0, apareça com o feedback de vazio
+  if (filteredCounter === 0) {
+    emptyFeedbackRef.classList.remove('hide');
+  }
+
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
 
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
