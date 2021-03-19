799d49b1756b70c9044d1dadbeb56e73f569cf5a
Thais Leal <thais.pll@hotmail.com>
2021-03-18 21:13:02 -0300

    add barra de cookies e arruma footer

--- js/filtro.js
+++ js/filtro.js
@@ -65,3 +65,18 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+(() => {
+  if (!localStorage.pureJavaScriptCookies) {
+    document.querySelector(".box-cookies").classList.remove('hide');
+  }
+  
+  const acceptCookies = () => {
+    document.querySelector(".box-cookies").classList.add('hide');
+    localStorage.setItem("pureJavaScriptCookies", "accept");
+  };
+  
+  const btnCookies = document.querySelector(".btn-cookies");
+  
+  btnCookies.addEventListener('click', acceptCookies);
+})();
